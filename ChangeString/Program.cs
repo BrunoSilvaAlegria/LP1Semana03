674ecb3a9846e8a089f76f689ad2a21795bbe04d
Bruno Alegria <bruno.alegria.lusofona@gmail.com>
2024-03-06 22:19:19 +0000

    Input: a string and a character

---
+++ ChangeString/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ChangeString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Asks the user for a string
+            Console.Write("Please write a string: >");
+            string str = Console.ReadLine();
+
+            //Asks the user for a character
+            Console.Write("Please choose a character: >");
+            string ch = Console.ReadLine();
+
+            
+        }
+    }
+}
