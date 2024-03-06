7f7efb0ef8f38f921082698f2d1fb27ad218b118
Bruno Alegria <bruno.alegria.lusofona@gmail.com>
2024-03-06 20:43:02 +0000

    str with >3 and <6 char, >6 = exit

--- RightSize/Program.cs
+++ RightSize/Program.cs
@@ -8,13 +8,14 @@ namespace RightSize
         {
             foreach (string str in args)
             {
-                if (str.Length > 3)
+                if (str.Length > 3 & str.Length <= 6)
                 {
                     Console.WriteLine(str);
                 }
-                
-                
-             
+                else if (str.Length > 6)
+                {
+                    return;
+                }
             }
         }
     }
