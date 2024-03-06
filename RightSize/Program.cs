1931c932687347c44174644604801b8ae7c75849
Bruno Alegria <bruno.alegria.lusofona@gmail.com>
2024-03-06 21:05:25 +0000

    added comments

--- RightSize/Program.cs
+++ RightSize/Program.cs
@@ -7,11 +7,15 @@ namespace RightSize
         static void Main(string[] args)
         {
             foreach (string str in args)
-            {
-                if (str.Length > 3 & str.Length <= 6)
+            {//Prints words with more than 3 characters and 
+             //less/equal to 6 characters
+                if (str.Length > 3 & str.Length <= 6) 
                 {
                     Console.WriteLine(str);
                 }
+                
+                //Detects if the word as more than 6 characters and 
+                //exits the program if true
                 else if (str.Length > 6)
                 {
                     return;
