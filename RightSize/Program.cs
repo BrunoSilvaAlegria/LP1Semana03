e03d25b288ed48710e5af4a8bef0a068ec36bccc
Bruno Alegria <bruno.alegria.lusofona@gmail.com>
2024-03-06 20:29:12 +0000

    string with more than 3 characters

---
+++ RightSize/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace RightSize
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (string str in args)
+            {
+                if (str.Length > 3)
+                {
+                    Console.WriteLine(str);
+                }
+                
+                
+             
+            }
+        }
+    }
+}
