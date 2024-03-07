c2cd104136353abde355626b695627d645f8f2c2
Bruno Alegria <bruno.alegria.lusofona@gmail.com>
2024-03-07 18:46:20 +0000

    added c

--- PowTwoUntil/Program.cs
+++ PowTwoUntil/Program.cs
@@ -6,10 +6,12 @@ namespace PowTwoUntil
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Calling this method twice despite its location
+            PowersOf2Until5();
+            PowersOf2Until5();
         }
-
-        private static void PowersOf2Until5()
+        
+        private static void PowersOf2Until5() //Added another method
         {
             for(int i = 1; i <= (1 << 5); i = i << 1)
             {
