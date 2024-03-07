2525b16f8466dc1854ed9d6c1f8f257c05352128
Bruno Alegria <bruno.alegria.lusofona@gmail.com>
2024-03-07 17:36:57 +0000

    added combinations and confirmations

--- HeroPerks/Program.cs
+++ HeroPerks/Program.cs
@@ -16,9 +16,9 @@ namespace HeroPerks
         {
             //Variable assigned to Perks and is going to be used as storage
             Perks HeroPerks = 0;
-        foreach (char c in args[0])
+        foreach (char ch in args[0])
         {   //Determines what happens if a 'wasd' letter is registered
-            switch (c)
+            switch (ch)
             {
                 case 'w':
                     HeroPerks ^= Perks.WaterBreathing;
@@ -41,6 +41,30 @@ namespace HeroPerks
                     Console.WriteLine("Unknown perk!");
                     return;
             }
+         
+        
+        //Detects if the player has no perks and prints exactly that
+        if (HeroPerks == 0)
+        {
+            Console.WriteLine("No perks at all!");
+        }
+        //Detects if the player has perks and prints them
+        else
+        {
+            Console.WriteLine($"Player perks: {HeroPerks}");
+        }
+        //Prints this combination
+        if ((HeroPerks & Perks.Stealth) == Perks.Stealth && 
+            (HeroPerks & Perks.DoubleJump) == Perks.DoubleJump)
+        {
+            Console.WriteLine("Silent jumper!");
+        }
+        //Confirms if AutoHeal is valid
+        if ((HeroPerks & Perks.AutoHeal) != Perks.AutoHeal)
+        {
+            Console.WriteLine("Not gonna make it!");
+        }
+        }
         }
 
         
