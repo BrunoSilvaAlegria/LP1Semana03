c9126e98d6b2173f80c21a3391f5e53b03998fa5
Bruno Alegria <bruno.alegria.lusofona@gmail.com>
2024-03-07 16:52:22 +0000

    Added the final matrix and all of the operations, and shows the result

--- ArrayMult/Program.cs
+++ ArrayMult/Program.cs
@@ -27,7 +27,29 @@ namespace ArrayMult
             matrixB[0,0] = float.Parse(args[4]);
             matrixB[1,0] = float.Parse(args[5]);
 
-            
+            //Matrix Result
+            float[,] matrix_result = new float[2, 1];
+            //Auxiliary variable
+            float A_plus_B = 0;
+
+            //Double cicle for that multiplies, adds and gives variables
+            for(int i = 0; i < 2; i++)
+            {
+                for(int j = 0; j < 2; j++)
+                {
+                    //Multiplication of values and adds the result 
+                    //to the auxiliary variable
+                    A_plus_B += matrixA[i, j] * matrixB[j, 0];
+                }
+                //Value assignment
+                matrix_result[i, 0] = A_plus_B;
+                //Auxiliary variable reset
+                A_plus_B = 0;
+            }
+
+            //Show all the values on the Matrix Result
+            foreach(float value in matrix_result)
+                    Console.WriteLine(value);
 
         }
     }
