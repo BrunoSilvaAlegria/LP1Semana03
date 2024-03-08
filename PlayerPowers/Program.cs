bdaac769d7322a45e65dbc8ef79bafb45dd44bd4
Bruno Alegria <bruno.alegria.lusofona@gmail.com>
2024-03-08 15:14:07 +0000

    added the players choice of powers and the special case

--- PlayerPowers/Program.cs
+++ PlayerPowers/Program.cs
@@ -5,26 +5,85 @@ namespace PlayerPowers
     [Flags]
     enum PlayerPowers
     {
-        Fly ,
-        XRayVision ,
+        Fly,
+        XRayVision,
         SuperStrength
     }
     class Program
     {
         static void Main(string[] args)
-        {   
+        {
             //Asks the player for the number of players
-            Console.WriteLine("How many players are playing?");
+            Console.Write("How many players are playing? > ");
             string str = Console.ReadLine();
 
             //Converts the number from string to int and uses it to determine 
             //the size of the array
             int n = int.Parse(str);
-            int[] array = new int[n];
+            PlayerPowers[] array = new PlayerPowers[n];
 
-           
+            //
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine("Powers: Fly, XRayVision, SuperStrength.");
+                while (true)
+                {
+                    Console.Write($"Player {n} what powers would you like? > ");
+                    string choice = Console.ReadLine();
+
+                    PlayerPowers Perk = 0;
+
+                    if (choice == "Fly")
+                    {
+                        Perk ^= PlayerPowers.Fly;
+
+                    }
+
+                    else if (choice == "Fly")
+                    {
+                        Perk ^= PlayerPowers.Fly;
+
+                    }
+
+                    else if (choice == "Fly")
+                    {
+                        Perk ^= PlayerPowers.Fly;
+
+                    }
+
+                    else
+                    {
+                        //If none of the options below is used, then it'll print this
+                        Console.WriteLine("Unknown perk!");
+                        return;
+                    }
+
+                    Console.WriteLine("Thats it?");
+                    Console.Write("Y -> Yes");
+                    Console.Write("N -> No");
+                    choice = Console.ReadLine();
+
+                    if (choice == "Y")
+                    {
+                        array[i] = Perk;
+                        break;
+                    }
 
 
+                }
+
+            }
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write($"O {i + 1}ª Player tem: {array[i]}");
+                if ((array[i] & PlayerPowers.SuperStrength) == PlayerPowers.SuperStrength
+                && (array[i] & PlayerPowers.Fly) == PlayerPowers.Fly)
+                {
+                    Console.Write(", Flying radiation");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
