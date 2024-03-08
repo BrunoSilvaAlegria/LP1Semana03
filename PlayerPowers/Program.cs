4f18c3aac0f957c33c1767552132c1bd1f643655
Bruno Alegria <bruno.alegria.lusofona@gmail.com>
2024-03-08 14:16:42 +0000

    added input, enum and conversion

---
+++ PlayerPowers/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace PlayerPowers
+{
+    [Flags]
+    enum PlayerPowers
+    {
+        Fly ,
+        XRayVision ,
+        SuperStrength
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            //Asks the player for the number of players
+            Console.WriteLine("How many players are playing?");
+            string str = Console.ReadLine();
+
+            //Converts the number from string to int and uses it to determine 
+            //the size of the array
+            int n = int.Parse(str);
+            int[] array = new int[n];
+
+           
+
+
+        }
+    }
+}
